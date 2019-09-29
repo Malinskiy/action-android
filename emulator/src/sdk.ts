c5bc07038191f7b8dee6aea57c25dfaaa737a7bc
Anton Malinskiy <anton@malinskiy.com>
2019-09-29 23:28:21 +1000

    fix(emulator): can't write to default sdk path. use home as parent

--- emulator/src/sdk.ts
+++ emulator/src/sdk.ts
@@ -2,16 +2,19 @@ import * as core from "@actions/core";
 import execWithResult from "./exec-with-result";
 
 const URL_LINUX = "https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip"
-const ANDROID_HOME = "/usr/local/lib/android/sdk"
 const ANDROID_TMP_PATH = "/tmp/android-sdk.zip"
 
 /**
  * Assuming that we will only run on Linux for now
  */
 export default async function installAndroidSdk(): Promise<boolean> {
+    let home = process.env.HOME;
+    const ANDROID_HOME = `${home}/android-sdk`
+
     await execWithResult(`curl -L ${URL_LINUX} -o ${ANDROID_TMP_PATH} -s`)
     await execWithResult(`unzip -q ${ANDROID_TMP_PATH} -d ${ANDROID_HOME}`)
     await execWithResult(`rm ${ANDROID_TMP_PATH}`)
+
 
     core.exportVariable('ANDROID_HOME', `${ANDROID_HOME}`);
     return true
