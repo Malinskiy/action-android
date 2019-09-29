3e2b873e5baef1ade5bd92e2380fcc9adabb5ff9
Anton Malinskiy <anton@malinskiy.com>
2019-09-29 21:05:48 +1000

    fix(emulator): add full path for sdkmanager

--- emulator/src/main.ts
+++ emulator/src/main.ts
@@ -14,7 +14,7 @@ async function run() {
 
     console.log(`Starting emulator with API=${api}, TAG=${tag} and ABI=${abi}...`)
 
-    await exec.exec(`sdkmanager "system-images;android-${api};${tag};${abi}" --verbose`);
+    await exec.exec(`$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-${api};${tag};${abi}" --verbose`);
   } catch (error) {
     core.setFailed(error.message);
   }
