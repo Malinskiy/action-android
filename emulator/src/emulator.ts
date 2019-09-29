c2d6c40c13510255ccb04468dee2920e6d501f06
Anton Malinskiy <anton@malinskiy.com>
2019-09-30 00:18:11 +1000

    fix(emulator): proper sdkmanager path

--- emulator/src/emulator.ts
+++ emulator/src/emulator.ts
@@ -2,7 +2,7 @@ import execWithResult from "./exec-with-result";
 import {androidHome} from "./sdk";
 
 export async function installEmulatorPackage(api: string, tag: string, abi: string): Promise<any> {
-    await execWithResult(`${androidHome()}/sdkmanager`, [`system-images;android-${api};${tag};${abi}`, "--verbose"]);
+    await execWithResult(`${androidHome()}/tools/bin/sdkmanager`, [`system-images;android-${api};${tag};${abi}`, "--verbose"]);
 }
 
 export async function createEmulator(name: string, api: string, tag: string, abi: string): Promise<any> {
