2c1d1f41acbd86a024b2b1da19473e4f402a61ad
Anton Malinskiy <anton@malinskiy.com>
2019-09-30 02:10:07 +1000

    fix(emulator): add required packages

--- emulator/src/emulator.ts
+++ emulator/src/emulator.ts
@@ -2,7 +2,7 @@ import execWithResult from "./exec-with-result";
 import {androidHome} from "./sdk";
 
 export async function installEmulatorPackage(api: string, tag: string, abi: string): Promise<any> {
-    await execWithResult(`${androidHome()}/tools/bin/sdkmanager`, [`system-images;android-${api};${tag};${abi}`, "--verbose"]);
+    await execWithResult(`${androidHome()}/tools/bin/sdkmanager`, ['emulator', 'tools', 'platform-tools', `system-images;android-${api};${tag};${abi}`, "--verbose"]);
 }
 
 export async function createEmulator(name: string, api: string, tag: string, abi: string): Promise<any> {
