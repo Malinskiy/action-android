cea1d16571afe5ea54600588e03a845a226eaba8
Anton Malinskiy <anton@malinskiy.com>
2019-09-29 23:23:15 +1000

    fix(emulator): quiet curl

--- emulator/src/sdk.ts
+++ emulator/src/sdk.ts
@@ -9,7 +9,7 @@ const ANDROID_TMP_PATH = "/tmp/android-sdk.zip"
  * Assuming that we will only run on Linux for now
  */
 export default async function installAndroidSdk(): Promise<boolean> {
-    await execWithResult(`curl -L ${URL_LINUX} > ${ANDROID_TMP_PATH}`)
+    await execWithResult(`curl -L ${URL_LINUX} -o ${ANDROID_TMP_PATH} -s`)
     await execWithResult(`unzip -q ${ANDROID_TMP_PATH} -d ${ANDROID_HOME}`)
     await execWithResult(`rm ${ANDROID_TMP_PATH}`)
 
