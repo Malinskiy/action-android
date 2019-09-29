850611a2fabfa81786470bd44ab13d30aacd9d27
Anton Malinskiy <anton@malinskiy.com>
2019-09-30 00:22:34 +1000

    fix(emulator): handle prompt for avdmanager

--- emulator/src/emulator.ts
+++ emulator/src/emulator.ts
@@ -6,11 +6,11 @@ export async function installEmulatorPackage(api: string, tag: string, abi: stri
 }
 
 export async function createEmulator(name: string, api: string, tag: string, abi: string): Promise<any> {
-    await execWithResult(`${androidHome()}/tools/bin/avdmanager create avd -n "${name}" --package "system-images;android-${api};${tag};${abi}" --tag ${tag}`)
+    await execWithResult(`echo no | ${androidHome()}/tools/bin/avdmanager create avd -n "${name}" --package "system-images;android-${api};${tag};${abi}" --tag ${tag}`)
 }
 
 export async function startEmulator(name: string): Promise<boolean> {
-    await execWithResult(`${androidHome()}/tools/emulator @${name}`)
+    await execWithResult(`${androidHome()}/tools/emulator @${name} &`)
     return await waitForBoot()
 }
 
