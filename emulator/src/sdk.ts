cf44abeec2e8a955ca9f5794743fdb83dc24ed66
Anton Malinskiy <amalinskiy@atlassian.com>
2019-10-02 01:12:10 +1000

    fix(emulator): fix quotes for package install

--- emulator/src/sdk.ts
+++ emulator/src/sdk.ts
@@ -76,11 +76,11 @@ abstract class BaseAndroidSdk implements AndroidSDK {
     }
 
     async installEmulatorPackage(api: string, tag: string, abi: string): Promise<any> {
-        await execWithResult(`bash -c \\\"${this.androidHome()}/tools/bin/sdkmanager emulator tools platform-tools system-images;android-${api};${tag};${abi} | uniq\"`);
+        await execWithResult(`bash -c \\\"${this.androidHome()}/tools/bin/sdkmanager emulator tools platform-tools 'system-images;android-${api};${tag};${abi}'"`);
     }
 
     async installPlatform(api: string): Promise<any> {
-        await execWithResult(`${this.androidHome()}/tools/bin/sdkmanager`, [`platforms;android-${api}"`])
+        await execWithResult(`${this.androidHome()}/tools/bin/sdkmanager`, [`platforms;android-${api}`])
     }
 
     async createEmulator(name: string, api: string, tag: string, abi: string): Promise<any> {
