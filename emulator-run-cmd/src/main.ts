ae5624953a57177ae42c4fe15df8a431177882a2
Anton Malinskiy <anton@malinskiy.com>
2019-10-04 20:47:54 +1000

    feat(emulator-run-cmd): add new combined action

---
+++ emulator-run-cmd/src/main.ts
@@ -0,0 +1,77 @@
+import * as core from '@actions/core';
+import {InputOptions} from "@actions/core/lib/core";
+import {SdkFactory} from "./sdk";
+import execWithResult from "./exec-with-result";
+
+async function run() {
+    try {
+        let api = core.getInput('api', <InputOptions>{required: false});
+        if (api == null || api == "") {
+            console.log(`API not set. Using 25`)
+            api = '25'
+        }
+
+        let abi = core.getInput('abi', <InputOptions>{required: false});
+        if (abi == null || abi == "") {
+            console.log(`ABI not set. Using armeabi-v7a`)
+            abi = 'armeabi-v7a'
+        }
+
+        let tag = core.getInput('tag', <InputOptions>{required: false})
+        if (tag !== "default" && tag !== "google_apis") {
+            console.log(`Unknown tag ${tag}. Using default`)
+            tag = 'google_apis'
+        }
+
+        let verbose = false
+        if (core.getInput('verbose') == "yes") {
+            verbose = true
+        }
+
+        let cmd = core.getInput('cmd', <InputOptions>{required: true})
+        if (cmd === "") {
+            console.error("Please specify cmd to execute in parallel with emulator")
+            return
+        }
+
+        console.log(`Starting emulator with API=${api}, TAG=${tag} and ABI=${abi}...`)
+
+        const androidHome = process.env.ANDROID_HOME
+        console.log(`ANDROID_HOME is ${androidHome}`)
+        console.log(`PATH is ${process.env.PATH}`)
+
+        let sdk = new SdkFactory().getAndroidSdk();
+
+        try {
+            await sdk.installEmulatorPackage(api, tag, abi, verbose)
+            await sdk.installPlatform(api, verbose)
+
+            let supportsHardwareAcceleration = await sdk.verifyHardwareAcceleration();
+            if (!supportsHardwareAcceleration && abi == "x86") {
+                core.setFailed('Hardware acceleration is not supported')
+                return
+            }
+
+            let emulator = await sdk.createEmulator("emulator", api, tag, abi);
+            console.log("starting adb server")
+            await sdk.startAdbServer()
+            await emulator.start()
+            console.log("emulator started and booted")
+
+            await execWithResult(`${cmd}`)
+
+            console.log("stopping emulator")
+            await emulator.stop()
+            console.log("emulator is stopped")
+        } catch (error) {
+            console.error(error)
+            core.setFailed(error.message);
+            return
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+        return
+    }
+}
+
+run();
