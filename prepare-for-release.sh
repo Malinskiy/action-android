c8d0096e9a91f7283d462f966b710276b75427ef
Anton Malinskiy <anton@malinskiy.com>
2019-11-08 21:04:25 +1100

    fix(build): fix type in prepare release script

--- prepare-for-release.sh
+++ prepare-for-release.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-for i in emulator-run-cmd, install-sdk; do
-  (cd $i && npm prune --production && git add node_modules)
+for i in emulator-run-cmd install-sdk; do
+  (cd $i && npm install && npm run build && npm prune --production && git add node_modules)
 done
