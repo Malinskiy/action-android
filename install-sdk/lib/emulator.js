7c2c3652a0c29f95f598dccf1e74cc1e02c547f1
Anton Malinskiy <anton@malinskiy.com>
2019-11-08 21:11:06 +1100

    chore(release): 0.0.3

---
+++ install-sdk/lib/emulator.js
@@ -0,0 +1,71 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const exec_with_result_1 = __importDefault(require("./exec-with-result"));
+class Emulator {
+    constructor(sdk, name, api, abi, tag, adbPort, telnetPort) {
+        this.sdk = sdk;
+        this.name = name;
+        this.api = api;
+        this.abi = abi;
+        this.tag = tag;
+        this.adbPort = adbPort;
+        this.telnetPort = telnetPort;
+    }
+    start() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield exec_with_result_1.default(`bash -c \\\"${this.sdk.androidHome()}/tools/emulator @${this.name} -no-snapshot-save &\"`);
+            let booted = yield this.waitForBoot();
+            console.log(`booted=${booted}`);
+            return;
+        });
+    }
+    stop() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield exec_with_result_1.default(`bash -c \\\"${this.sdk.androidHome()}/platform-tools/adb -s emulator-${this.adbPort} emu kill\"`);
+            console.log("emu kill finished");
+            return;
+        });
+    }
+    waitForBoot() {
+        return __awaiter(this, void 0, void 0, function* () {
+            for (let countdown = 120; countdown > 0; countdown--) {
+                if (countdown == 0) {
+                    console.error("Timeout waiting for the emulator");
+                    return false;
+                }
+                try {
+                    let output = yield exec_with_result_1.default(`${this.sdk.androidHome()}/platform-tools/adb shell getprop sys.boot_completed`);
+                    if (output.trim() == '1') {
+                        countdown = 0;
+                        console.log("Emulator booted");
+                        return true;
+                    }
+                }
+                catch (e) {
+                    console.error(e.message);
+                }
+                console.log("Sleeping for 1s");
+                yield sleep(1000);
+                countdown--;
+            }
+            console.log("Timeout waiting for emulator to boot. Exiting");
+            return false;
+        });
+    }
+}
+exports.Emulator = Emulator;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
--- prepare-for-release.sh
+++ prepare-for-release.sh
@@ -3,5 +3,5 @@
 set -ex
 
 for i in emulator-run-cmd install-sdk; do
-  (cd $i && npm install && npm run build && npm prune --production && git add node_modules)
+  (cd $i && npm install && npm run build && npm prune --production && git add -f node_modules lib)
 done
