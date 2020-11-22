ee2c2410841231c509b93845d7cfe0d8b1e4e47b
Hannes Achleitner <hannes.software@gmx.at>
2020-11-22 11:21:31 +0100

    Release CI

--- prepare-for-release.sh
+++ prepare-for-release.sh
@@ -3,5 +3,5 @@
 set -ex
 
 for i in emulator-run-cmd install-sdk; do
-  (cd $i && docker run -it -v $(pwd):/opt/app -w /opt/app node:16 bash -c 'npm install && npm run build && npm prune --production' && git add -f node_modules lib)
+  (cd $i && docker run -t -v $(pwd):/opt/app -w /opt/app node:16 bash -c 'npm install && npm run build && npm prune --production' && git add -f node_modules lib)
 done
