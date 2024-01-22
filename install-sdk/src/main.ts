3a09eaf84467f353b2cab93280d9dda66e731e93
Anton Malinskiy <anton@malinskiy.com>
2024-01-22 15:00:05 +1000

    feat(sdk): accept licenses before installing cmdlines package

--- install-sdk/src/main.ts
+++ install-sdk/src/main.ts
@@ -14,13 +14,12 @@ async function run() {
             core.setFailed('You can\'t use this unless you accept the Android SDK licenses')
             return
         }
-
-        console.log("Installing Android Sdk")
         let sdk = new SdkFactory().getAndroidSdk();
-        await sdk.install(url)
-
         console.log("Accepting Android SDK licenses")
         await sdk.acceptLicense()
+        console.log("Installing Android Sdk")
+        await sdk.install(url)
+
     } catch (error) {
         if(error !instanceof Error) {
             core.setFailed(error.message);
